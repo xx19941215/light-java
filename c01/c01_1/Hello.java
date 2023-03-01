1553102e3df8da1431d255b23ca63f99e338f151
Xiao <xx19931215@gamil.com>
2023-03-01 20:40:14 +0800

    update

---
+++ c01/c01_1/Hello.java
@@ -0,0 +1,19 @@
+//文件中有中文，如何处理
+//设置文件编码GBK即可
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello,world");
+    }
+}
+
+class Dog {
+    public static void main(String [] args) {
+        System.out.println("hello，小狗狗");
+    }
+}
+
+class Tiger {
+    public static void main(String[] args) {
+        System.out.println("hello，小老虎");
+    }
+}
