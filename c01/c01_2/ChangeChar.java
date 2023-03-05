caff6d8b97b7622eebbdb28b5ffdb28cf14e2b54
Xiao <xx19931215@gamil.com>
2023-03-05 16:06:27 +0800

    Java转义字符

---
+++ c01/c01_2/ChangeChar.java
@@ -0,0 +1,14 @@
+//Java转义字符
+
+public class ChangeChar {
+    public static void main(String[] args) {
+        //\t 制表位，实现对齐功能
+        System.out.println("北京\t天津\t上海");
+        //\n 换行符
+        System.out.println("jack\nsmith\nmary");
+        //\\ 一个\
+        System.out.println("\\n");
+        //\r：一个回车
+        System.out.println("韩顺平教育\r北京");
+    }
+}
