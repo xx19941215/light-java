dd2efaedee4d42a71cc464ccc5d166e1da3dffcc
Xiao <xx19931215@gamil.com>
2023-05-11 20:26:03 +0800

    String转基本数据类型

---
+++ c01/c01_6/StringToBasicDetail.java
@@ -0,0 +1,9 @@
+public class StringToBasicDetail {
+    public static void main(String[] args) {
+        String str = "hello";
+
+        int n1 = Integer.parseInt(str);
+
+        System.out.println(n1);
+    }
+}
